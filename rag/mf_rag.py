bf90720219e8deae5f9154eaff111d10c8d5f973
VRazgaitis <vaidas.razgaitis@gmail.com>
2024-03-06 09:52:34 -0600

    updating rag code

--- rag/mf_rag.py
+++ rag/mf_rag.py
@@ -76,12 +76,7 @@ def getanswer(query):
     for chunk in relevant_chunks:
         chunk_docs.append(chunk[0])
     results = chain({"input_documents": chunk_docs, "question": query})
-    # text_reference=""
-    # for i in range(len(results["input_documents"])):
-    #     text_reference+=results["input_documents"][i].page_content
-    # output={"Answer":results["output_text"],"Reference":text_reference}
     output={"Answer":results["output_text"]}
-    # return output
     return results["output_text"]
 
 # Streamlit app
