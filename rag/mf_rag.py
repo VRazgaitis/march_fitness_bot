403bc015e6759e995ee107c85d50af4ee2860305
VRazgaitis <vaidas.razgaitis@gmail.com>
2024-03-05 23:03:11 -0600

    debugging

--- rag/mf_rag.py
+++ rag/mf_rag.py
@@ -95,4 +95,4 @@ def getanswer(query):
 if __name__ == "__main__":
     st.title("March fitness rules chatbot")
     question = st.text_input("Enter a question about rules or scoring in March fitness:")
-    st.write("MF admin chatbot says:", getanswer(question))
+    st.write("🦾MF admin commitee🦾:", getanswer(question))
